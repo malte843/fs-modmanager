dd737786b7f28fa6192a6fc0ad2f58f6b5e8b39a
malte <41925758+malte843@users.noreply.github.com>
2023-01-20 17:41:29 +0100

    Add files via upload

---
+++ ModManager/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace ModManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSModfolder = new System.Windows.Forms.Button();
+            this.btnSModPacks = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bgwCreatePack = new System.ComponentModel.BackgroundWorker();
+            this.bgwLoadPack = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(539, 224);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(600, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(600, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnSModfolder
+            // 
+            this.btnSModfolder.Location = new System.Drawing.Point(618, 34);
+            this.btnSModfolder.Name = "btnSModfolder";
+            this.btnSModfolder.Size = new System.Drawing.Size(107, 26);
+            this.btnSModfolder.TabIndex = 3;
+            this.btnSModfolder.Text = "Select";
+            this.btnSModfolder.UseVisualStyleBackColor = true;
+            this.btnSModfolder.Click += new System.EventHandler(this.btnSModfolder_Click);
+            // 
+            // btnSModPacks
+            // 
+            this.btnSModPacks.Location = new System.Drawing.Point(618, 90);
+            this.btnSModPacks.Name = "btnSModPacks";
+            this.btnSModPacks.Size = new System.Drawing.Size(107, 26);
+            this.btnSModPacks.TabIndex = 4;
+            this.btnSModPacks.Text = "Select";
+            this.btnSModPacks.UseVisualStyleBackColor = true;
+            this.btnSModPacks.Click += new System.EventHandler(this.btnSModPacks_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FS Modfolder path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Modpacks path";
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(557, 302);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(123, 23);
+            this.btnReload.TabIndex = 7;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(557, 170);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 34);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Create new Pack from Modfolder";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Enabled = false;
+            this.btnLoad.Location = new System.Drawing.Point(557, 274);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(123, 23);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Load Selected";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(557, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 26);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(557, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Pack name";
+            // 
+            // bgwCreatePack
+            // 
+            this.bgwCreatePack.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwCreatePack_DoWork);
+            this.bgwCreatePack.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwCreatePack_RunWorkerCompleted);
+            // 
+            // bgwLoadPack
+            // 
+            this.bgwLoadPack.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwLoadPack_DoWork);
+            this.bgwLoadPack.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwLoadPack_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(572, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Copying Files... | / - \\ ";
+            this.label4.Visible = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(557, 245);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete Selected";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Enabled = false;
+            this.btnShow.Location = new System.Drawing.Point(557, 216);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(123, 23);
+            this.btnShow.TabIndex = 15;
+            this.btnShow.Text = "Show in Explorer";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(737, 364);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSModPacks);
+            this.Controls.Add(this.btnSModfolder);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ModManager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSModfolder;
+        private System.Windows.Forms.Button btnSModPacks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bgwCreatePack;
+        private System.ComponentModel.BackgroundWorker bgwLoadPack;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ ModManager/Form1.cs
@@ -0,0 +1,294 @@
+﻿using ModManager.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ModManager
+{
+    public partial class Form1 : Form
+    {
+        private string modFolderPath = "";
+        private string modPacksPath = "";
+        private Boolean startup = true;
+        private string selectedItem;
+        private Boolean running = false;
+        private Boolean reload = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (startup)
+            {
+                Settings.Default.Save();
+                Settings.Default.Upgrade();
+                Settings.Default.Reload();
+                ReloadSettings();
+                textBox1.Text = modFolderPath;
+                textBox2.Text = modPacksPath;
+                ReloadModpacks();
+                startup = false;
+                timer1.Start();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Trim() != "")
+            {
+                running = true;
+                bgwCreatePack.RunWorkerAsync();
+            } else
+            {
+                MessageBox.Show("Name cant be empty");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            running = true;
+            bgwLoadPack.RunWorkerAsync();
+        }
+
+        private void ReloadSettings()
+        {
+            modFolderPath = Settings.Default.modFolderPath;
+            modPacksPath = Settings.Default.modPacksPath;
+            if (modPacksPath == "undefined" || modPacksPath.Trim() == "")
+            {
+                modPacksPath = Application.StartupPath + "\\modpacks";
+                SaveSettings();
+            }
+
+            if (modFolderPath == "undefined" || modFolderPath.Trim() == "")
+            {
+                modFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\FarmingSimulator2022\\mods";
+                SaveSettings();
+            }
+        }
+
+        private void SaveSettings()
+        {
+            Settings.Default.modFolderPath = modFolderPath;
+            Settings.Default.modPacksPath = modPacksPath;
+            Settings.Default.Save();
+            Settings.Default.Upgrade();
+            Settings.Default.Reload();
+        }
+
+        private void setModFolder(string path)
+        {
+            modFolderPath = path;
+            textBox1.Text = path;
+            SaveSettings();
+        }
+
+        private void setModPacks(string path)
+        {
+            modPacksPath = path;
+            textBox2.Text = path;
+            SaveSettings();
+        }
+
+        private void ReloadModpacks()
+        {
+            if (!Directory.Exists(modPacksPath))
+            {
+                Directory.CreateDirectory(modPacksPath);
+            }
+            listBox1.Items.Clear();
+            foreach (string dir in Directory.GetDirectories(modPacksPath))
+            {
+                listBox1.Items.Add(dir.Replace(modPacksPath + "\\", ""));
+            }
+        }
+
+        private void btnSModfolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                setModFolder(dialog.SelectedPath);
+            }
+        }
+
+        private void btnSModPacks_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                setModPacks(dialog.SelectedPath);
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            ReloadModpacks();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string selectedPackPath = modPacksPath + "\\" + selectedItem;
+
+            if (Directory.Exists(selectedPackPath))
+            {
+                DirectoryInfo directory = new DirectoryInfo(selectedPackPath);
+                foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
+                foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+                Directory.Delete(selectedPackPath);
+
+                reload = true;
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string selectedPackPath = modPacksPath + "\\" + selectedItem;
+            if (Directory.Exists(selectedPackPath))
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        Arguments = selectedPackPath,
+                        FileName = "explorer.exe"
+                    };
+                    Process.Start(startInfo);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void bgwCreatePack_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (textBox3.Text.Trim() != "")
+            {
+                Directory.CreateDirectory(modPacksPath + "\\" + textBox3.Text);
+                new Microsoft.VisualBasic.Devices.Computer().FileSystem.CopyDirectory(modFolderPath, modPacksPath + "\\" + textBox3.Text);
+                reload = true;
+            }
+        }
+
+        private void bgwLoadPack_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string selectedPackPath = modPacksPath + "\\" + selectedItem;
+            if (Directory.Exists(selectedPackPath))
+            {
+                if (!Directory.Exists(modFolderPath))
+                    Directory.CreateDirectory(modFolderPath);
+
+                DirectoryInfo directory = new DirectoryInfo(modFolderPath);
+                foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
+                foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+
+                new Microsoft.VisualBasic.Devices.Computer().FileSystem.CopyDirectory(selectedPackPath, modFolderPath);
+                MessageBox.Show("Loaded Pack " + selectedItem);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                selectedItem = listBox1.SelectedItem.ToString();
+                btnLoad.Enabled = true;
+                btnDelete.Enabled = true;
+                btnShow.Enabled = true;
+            } else
+            {
+                selectedItem = "";
+                btnLoad.Enabled = false;
+                btnDelete.Enabled = false;
+                btnShow.Enabled = false;
+            }
+        }
+
+        private void bgwLoadPack_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            running = false;
+        }
+
+        private void bgwCreatePack_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            running = false;
+        }
+
+        int tickCount = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (reload)
+            {
+                ReloadModpacks();
+                reload = false;
+            }
+                
+
+            // Loading... | / - \ 
+            if (running)
+            {
+                label4.Visible = true;
+                if (tickCount == 0)
+                {
+                    label4.Text = "Copying Files... |";
+                    tickCount = 1;
+                }
+                else if (tickCount == 1)
+                {
+                    label4.Text = "Copying Files... /";
+                    tickCount = 2;
+                }
+                else if (tickCount == 2)
+                {
+                    label4.Text = "Copying Files... -";
+                    tickCount = 3;
+                } else
+                {
+                    label4.Text = "Copying Files... \\";
+                    tickCount = 0;
+                }
+            } else
+            {
+                label4.Visible = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.modFolderPath = modFolderPath;
+            Settings.Default.modPacksPath = modPacksPath;
+            Settings.Default.Save();
+            Settings.Default.Upgrade();
+            Settings.Default.Reload();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsNumber(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Space || char.IsControl(e.KeyChar))
+            {
+                // These characters may pass
+                e.Handled = false;
+            }
+            else
+            {
+                // Everything that is not a letter, nor a backspace nor a space will be blocked
+                e.Handled = true;
+            }
+        }
+    }
+}
